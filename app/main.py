4c7325ec51b7cbfdcb29cf2a3c49c1165cf636f9
SANJAY.K <ksanjayhannah16@gmail.com>
2026-09-23 01:47:02 +0530

    Add nova-frontend origin to CORS middleware

--- app/main.py
+++ app/main.py
@@ -46,6 +46,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
+        "https://nova-frontend-qf6r.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
